132677be998015f93c9271c09ac41d9133ea185f
Fedya <azizsaidazizov2@gmail.com>
2026-02-24 10:40:19 +0500

    sa

---
+++ app.js
@@ -0,0 +1,40 @@
+fetch("http://localhost:8000/top")
+  .then(r => r.json())
+  .then(data => {
+    const root = document.getElementById("top");
+    root.innerHTML = "";
+
+    data.forEach((u, i) => {
+
+      const div = document.createElement("div");
+      div.className = "user__wrapper";
+
+      div.innerHTML = `
+        <p class="place">${i + 1}</p>
+
+        <div class="user__img">
+          <img src="${u.avatar ? u.avatar : 'as.png'}" alt="">
+        </div>
+
+        <div class="user__top">
+            <b class="user__info">${u.name}</b>
+            <span class="user__stats">${u.count} msgs</span>
+        </div>
+      `;
+
+      root.appendChild(div);
+    });
+  })
+  .catch(err => {
+    console.error("Ошибка загрузки:", err);
+  });
+
+
+  const items = document.querySelectorAll(".nav__item");
+
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        document.querySelector(".nav__item.active")?.classList.remove("active");
+        item.classList.add("active");
+    });
+});
